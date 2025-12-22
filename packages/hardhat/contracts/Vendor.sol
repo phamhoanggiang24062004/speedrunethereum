11fa7046cac2673989f67bb69e348c43cede9d9a
phamhoanggiang24062004 <137171900+phamhoanggiang24062004@users.noreply.github.com>
2025-12-22 23:10:57 +0700

    create buyTokens, sellTokens and withdraw function

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,21 +1,54 @@
-pragma solidity 0.8.20; //Do not change the solidity version as it negatively impacts submission grading
+pragma solidity 0.8.20;
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
-import "./YourToken.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Vendor {
-    // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+contract Vendor is Ownable {
 
-    YourToken public yourToken;
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
+
+    IERC20 public yourToken;
+
+    uint256 public constant tokensPerEth = 100;
 
     constructor(address tokenAddress) {
-        yourToken = YourToken(tokenAddress);
+        yourToken = IERC20(tokenAddress);
     }
 
-    // ToDo: create a payable buyTokens() function:
+    // Buy tokens with ETH
+    function buyTokens() external payable {
+        require(msg.value > 0, "Send ETH to buy tokens");
 
-    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+        uint256 tokensToSend = msg.value * tokensPerEth;
+        require(
+            yourToken.balanceOf(address(this)) >= tokensToSend,
+            "Insufficient tokens in vendor"
+        );
 
-    // ToDo: create a sellTokens(uint256 _amount) function:
+        yourToken.transfer(msg.sender, tokensToSend);
+
+        emit BuyTokens(msg.sender, msg.value, tokensToSend);
+    }
+
+    // Sell tokens to get ETH back
+    function sellTokens(uint256 tokenAmount) external {
+        require(tokenAmount > 0, "Specify tokens to sell");
+
+        uint256 ethToSend = tokenAmount / tokensPerEth;
+        require(address(this).balance >= ethToSend, "Vendor has insufficient ETH");
+
+        // User must approve Vendor first
+        yourToken.transferFrom(msg.sender, address(this), tokenAmount);
+
+        payable(msg.sender).transfer(ethToSend);
+
+        emit SellTokens(msg.sender, tokenAmount, ethToSend);
+    }
+
+    // Owner withdraws ETH
+    function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
 }
